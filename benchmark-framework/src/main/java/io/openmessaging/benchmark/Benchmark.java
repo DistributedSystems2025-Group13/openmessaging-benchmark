f29bdb46dd31f8995b272032ac9a9422679ce9e6
NFJJunior <iancu_ivasciuc@yahoo.com>
2025-12-21 05:22:11 +0100

    fix: print json data when no output file is provided

--- benchmark-framework/src/main/java/io/openmessaging/benchmark/Benchmark.java
+++ benchmark-framework/src/main/java/io/openmessaging/benchmark/Benchmark.java
@@ -152,7 +152,8 @@ public class Benchmark {
 
                     TestResult result = generator.run();
 
-                    String fileName = arguments.output.length() > 0 ? arguments.output
+                    String fileName = (arguments.output != null && !arguments.output.isEmpty()) 
+                            ? arguments.output
                             : String.format("%s-%s-%s.json", workloadName, driverConfiguration.name,
                                     dateFormat.format(new Date()));
 
